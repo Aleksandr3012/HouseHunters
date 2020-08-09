aebb32c3cc3770b6778944b8e1a9529a61760abd
Aleksandr3012 <flaming541@gmail.com>
2020-08-09 20:39:14 +0300

    for pull

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -230,7 +230,7 @@ function eventHandler() {
 	// JSCCommon.CustomInputFile();
 	// добавляет подложку для pixel perfect
 	let screenName;
-	screenName = '07.jpg';
+	screenName = '08.jpg';
 	screenName
 		? $(".main-wrapper").after(`<div class="pixel-perfect" style="background-image: url(screen/${screenName});"></div>`)
 		: '';
